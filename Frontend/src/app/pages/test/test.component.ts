a021ab2765fd6aaa42766dfac391b1656a0f7217
Matz Henriksen <matz@mudah.no>
2024-03-12 10:35:15 +0100

    added a chart module and tests.

--- Frontend/src/app/pages/test/test.component.ts
+++ Frontend/src/app/pages/test/test.component.ts
@@ -5,6 +5,9 @@ import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { StyleClassModule } from 'primeng/styleclass';
 import { LoggerService } from '../../services/logger.service';
+import { AgChartsAngular } from 'ag-charts-angular';
+import { AgChartOptions } from 'ag-charts-community';
+import { ChartComponent } from '../../components/chart/chart.component';
 
 @Component({
   selector: 'app-test',
@@ -14,6 +17,8 @@ import { LoggerService } from '../../services/logger.service';
     StyleClassModule, 
     TranslateModule, 
     RippleModule,
+    AgChartsAngular,
+    ChartComponent
   ],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
@@ -21,10 +26,27 @@ import { LoggerService } from '../../services/logger.service';
 export class TestComponent {
   readonly #logger = inject(LoggerService);
   http = inject(HttpClient);
+
+  public chartOptions: AgChartOptions;
+
   constructor() {
     this.http.get('https://jsonplaceholder.typicode.com/users')
     .subscribe((res) => {
       this.#logger.log('res:', res);
     });
+
+    this.chartOptions = {
+      // Data: Data to be displayed in the chart
+      data: [
+        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
+        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
+        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
+        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
+        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
+        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
+      ],
+      // Series: Defines which chart type and data to use
+      series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }]
+    };
   }
 }
