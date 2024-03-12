1043a3e899afc8d43cdb40895161d000e439ec03
Aleksander Sveøy-Aae <aleksander.aae@redox.no>
2024-03-12 09:02:48 +0100

    General working CSV to JSON code.

--- ProjectPidgeon.py
+++ ProjectPidgeon.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import json
 
-def excel_to_json_pandas(excel_file, sheet_name="Sheet1"):
-  df = pd.read_excel(excel_file, sheet_name=sheet_name)
+def csv_to_json_pandas(csv_file):
+  df = pd.read_csv(csv_file)
   json_data = df.to_dict(orient='records')
 
   return json_data
 
-excel_file = "test.csv"
-json_data = excel_to_json_pandas(excel_file)
+csv_file = "Oppdrett.csv"
+json_data = csv_to_json_pandas(csv_file)
 
-# Print the JSON data
 print(json.dumps(json_data, indent=4))
+#EXPORT DATA AS JSON
